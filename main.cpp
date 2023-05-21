85c66b8e9eaf6bd96ddf2bef439aba3a088cc5aa
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-05-21 23:50:21 +0400

    slightly worse age calc

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+#include <chrono>
 #include <ctime>
 #include <iostream>
+#include <math.h>
 #include <string>
 
 using namespace std;
@@ -20,20 +22,28 @@ string genderStr(Gender gender) {
   return gender == Gender::MALE ? "Male" : "Female";
 }
 
+int getYearDiff(tm start) {
+
+  time_t x = mktime(&start);
+  time_t y = time(nullptr);
+
+  if (x != (std::time_t)(-1) && y != (std::time_t)(-1)) {
+    return floorf(std::difftime(y, x) / (60 * 60 * 24 * 365.25));
+  }
+
+  return -1;
+}
+
+string getFormattedDate(tm date) {
+  return padLeft(to_string(date.tm_mday), '0', 2) + "/" +
+         padLeft(to_string(date.tm_mon + 1), '0', 2) + "/" +
+         to_string(date.tm_year + 1900);
+}
+// utility functions
+
 struct Stock {
   string code;
   uint price;
-};
-
-struct Date {
-  int day;
-  int month;
-  int year;
-
-  string getFormattedDate() {
-    return padLeft(to_string(day), '0', 2) + "/" +
-           padLeft(to_string(month), '0', 2) + "/" + to_string(year);
-  }
 };
 
 class InvestmentAccout {
@@ -51,22 +61,18 @@ protected:
   string name;
   bool isCitizen;
   Gender gender;
-  Date *birthday;
+  tm birthday;
   InvestmentAccout *investmentAccout;
 
 public:
   User(string id, string name, int age, bool isCitizen, Gender gender,
-       Date *birthday)
+       tm birthday)
       : id(id), name(name), isCitizen(isCitizen), gender(gender),
         birthday(birthday) {}
 
   string getId() { return this->id; }
 
-  int getAge() {
-    time_t t = time(nullptr);
-    tm *const pTInfo = localtime(&t);
-    return 1900 + pTInfo->tm_year - this->birthday->year;
-  }
+  int getAge() { return getYearDiff(this->birthday); }
 
   void logBasicData() {
     cout << "id: " << this->id << endl;
@@ -74,22 +80,19 @@ public:
     cout << "age: " << this->getAge() << endl;
     cout << "is a citizen: " << prettyBool(this->isCitizen) << endl;
     cout << "gender: " << genderStr(this->gender) << endl;
-    cout << "birthday: " << this->birthday->getFormattedDate() << endl;
+    cout << "birthday: " << getFormattedDate(this->birthday) << endl;
   }
 
   void attachInvestmentAccount(InvestmentAccout *acc) {
     this->investmentAccout = acc;
   }
 
-  ~User() {
-    delete birthday;
-    delete investmentAccout;
-  }
+  ~User() { delete investmentAccout; }
 };
 
 int main() {
   User *user = new User("100", "Nika Qvrivishvili", 19, true, Gender::MALE,
-                        new Date{26, 3, 2004});
+                        {0, 0, 0, 26, 2, 104});
   user->logBasicData();
   user->attachInvestmentAccount(new InvestmentAccout(user->getId()));
   delete user;
