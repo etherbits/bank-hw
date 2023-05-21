4acc2ac8294172282305c60163c46acfbad60a6d
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-05-21 17:29:13 +0400

    adding User class

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string padLeft(string str, char padChar, int len) {
+  while (str.length() < len) {
+    str.insert(0, 1, padChar);
+  }
+  return str;
+}
+
+struct Date {
+  int day;
+  int month;
+  int year;
+
+  string getFormattedDate() {
+    return padLeft(to_string(day), '0', 2) + "/" +
+           padLeft(to_string(month), '0', 2) + "/" +
+           padLeft(to_string(year), '0', 2);
+  }
+};
+
+class User {
+  string id;
+  string name;
+  int age;
+  bool isCitizen;
+  string gender;
+  Date *birthday;
+
+public:
+  User(string id, string name, int age, bool isCitizen, string gender,
+       Date *birthday)
+      : id(id), name(name), age(age), isCitizen(isCitizen), gender(gender),
+        birthday(birthday) {}
+
+  void logBasicData() {
+    cout << "id: " << this->id << endl;
+    cout << "name: " << this->name << endl;
+    cout << "age: " << this->age << endl;
+    cout << "is a citizen: " << this->isCitizen << endl;
+    cout << "gender: " << this->gender << endl;
+    cout << "birthday: " << this->birthday->getFormattedDate() << endl;
+  }
+};
+
+int main() {
+  User *user = new User("100", "Nika", 19, true, "Male", new Date{26, 3, 2004});
+  user->logBasicData();
+  return 0;
+}
