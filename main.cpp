0de02bb12613c1299012085d4d87d9f63cd446ef
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-05-22 19:58:08 +0400

    changing flow

--- main.cpp
+++ main.cpp
@@ -45,13 +45,20 @@ string getFormattedDate(tm date) {
 struct Stock {
   string company;
   string code;
+};
+
+struct StockListing : public Stock {
   double price;
 };
 
-void logStocks(map<string, Stock> stocks) {
+struct OwnedStock : public Stock {
+  double amount;
+};
+
+void logStocks(map<string, StockListing> stocks) {
   cout << endl << "=== Available Stocks ===" << endl;
 
-  for (std::map<string, Stock>::iterator it = stocks.begin();
+  for (std::map<string, StockListing>::iterator it = stocks.begin();
        it != stocks.end(); ++it) {
     cout << "Company: " << it->second.company << " || "
          << "Code: " << it->second.code << " || "
@@ -62,9 +69,45 @@ void logStocks(map<string, Stock> stocks) {
 
 class InvestmentAccout {
   string userId;
+  map<string, OwnedStock> ownedStocks;
 
 public:
   InvestmentAccout(string userId) : userId(userId) {}
+
+  void buyStock(string code, double payment,
+                map<string, StockListing> availableStocks) {
+    // check if stock exists
+    if (availableStocks.find(code) == availableStocks.end()) {
+      return;
+    }
+
+    // check if user owns stock
+    if (ownedStocks.find(code) == ownedStocks.end()) {
+      this->ownedStocks[code] = {availableStocks[code].company,
+                                 availableStocks[code].code,
+                                 payment / availableStocks[code].price};
+
+      return;
+    }
+
+    this->ownedStocks[code].amount += payment / availableStocks[code].price;
+
+    cout << code << this->ownedStocks[code].amount << endl;
+  }
+
+  void logStocks() {
+
+    cout << endl << "=== Owned Stocks ===" << endl;
+
+    for (std::map<string, OwnedStock>::iterator it = this->ownedStocks.begin();
+         it != ownedStocks.end(); ++it) {
+      cout << "Company: " << it->second.company << " || "
+           << "Code: " << it->second.code << " || "
+           << "Amount: " << it->second.amount << endl;
+    }
+    cout << endl;
+  }
+
   void log() { cout << this->userId << endl; }
 };
 
@@ -79,12 +122,9 @@ protected:
   InvestmentAccout *investmentAccout;
 
 public:
-  User(string id, string name, int age, bool isCitizen, Gender gender,
-       tm birthday)
+  User(string id, string name, bool isCitizen, Gender gender, tm birthday)
       : id(id), name(name), isCitizen(isCitizen), gender(gender),
-        birthday(birthday) {}
-
-  string getId() { return this->id; }
+        birthday(birthday), investmentAccout(nullptr) {}
 
   int getAge() { return getYearDiff(this->birthday); }
 
@@ -95,44 +135,60 @@ public:
     cout << "is a citizen: " << prettyBool(this->isCitizen) << endl;
     cout << "gender: " << genderStr(this->gender) << endl;
     cout << "birthday: " << getFormattedDate(this->birthday) << endl;
+    if (this->investmentAccout != nullptr) {
+      this->investmentAccout->logStocks();
+    }
   }
 
-  void attachInvestmentAccount(InvestmentAccout *acc) {
-    this->investmentAccout = acc;
+  void createInvestmentAccount() {
+    this->investmentAccout = new InvestmentAccout(this->id);
+  }
+
+  void buyStock(string code, double payment,
+                map<string, StockListing> availableStocks) {
+    this->investmentAccout->buyStock(code, payment, availableStocks);
   }
 
   ~User() { delete investmentAccout; }
 };
 
-void logUsers(vector<User *> users) {
+void logUsers(vector<User> &users) {
   cout << endl << "=== All Customers ===" << endl;
   for (int i = 0; i < users.size(); i++) {
     cout << "---" << endl;
-    users[i]->logBasicData();
+    users[i].logBasicData();
   }
   cout << endl;
 }
 
 int main() {
-  map<string, Stock> stocks = {{"AMZN", {"Amazon.com Inc.", "AMZN", 114.8}},
-                               {"NFLX", {"Netflix Inc.", "NFLX", 367.4}},
-                               {"NVDA", {"Nvidia Corp.", "NVDA", 313.2}}};
-  vector<User *> users = {};
+  // stock listings
+  map<string, StockListing> stocks = {
+      {"AMZN", {"Amazon.com Inc.", "AMZN", 114.8}},
+      {"NFLX", {"Netflix Inc.", "NFLX", 367.4}},
+      {"NVDA", {"Nvidia Corp.", "NVDA", 313.2}}};
 
-  User *user1 = new User("100", "Nika Qvrivishvili", 19, true, Gender::MALE,
-                         {0, 0, 0, 26, 2, 104});
+  // users
+  vector<User> users = {};
 
-  User *user2 =
-      new User("123", "John Doe", 19, true, Gender::MALE, {0, 0, 0, 22, 4, 99});
+  users.push_back({"01010101010",
+                   "Nika Qvrivishvili",
+                   true,
+                   Gender::MALE,
+                   {0, 0, 0, 26, 2, 104}});
 
-  users.push_back(user1);
-  users.push_back(user2);
+  users.push_back(
+      {"01010101011", "John Doe", true, Gender::MALE, {0, 0, 0, 22, 3, 99}});
 
-  user1->attachInvestmentAccount(new InvestmentAccout(user1->getId()));
+  users[0].createInvestmentAccount();
+
+  users[0].buyStock("AMZN", 100, stocks);
+  users[0].buyStock("AMZN", 200, stocks);
+
+  users[0].buyStock("NFLX", 200, stocks);
 
   logStocks(stocks);
   logUsers(users);
 
-  delete user1;
   return 0;
 }
